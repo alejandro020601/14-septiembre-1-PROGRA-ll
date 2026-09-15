048f82894264de335a45c67998cbfa504b667c02
ALE Universidad <ALE Universidad@FAMAGUILARARCE>
2026-09-14 20:46:55 -0600

    Add project files.

---
+++ 14 septiembre #1 PROGRA ll/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_septiembre__1_PROGRA_ll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+
+            int num3, num4;
+            float flotante = 3.14f;
+            string cadena = "Hola mundo";
+            String cadena2 = "Hola mundo 2";
+            char caracter = 'A';
+            Boolean booleano;
+            bool booleano2 = true;
+
+            var variable = 20; // tipo de dato inferido
+
+            dynamic dinamica = null; // tipo de dato dinámico
+            dinamica = "Hola mundo"; // se puede cambiar el tipo de dato en tiempo de ejecución
+            dinamica = 3.14; // se puede cambiar el tipo de dato en tiempo de ejecución
+            dinamica = true; // se puede cambiar el tipo de dato en tiempo de ejecución
+
+            string nombre = "Alejandro ";
+            Console.WriteLine("Yo me llamo: " + nombre);
+            string apellido;
+            Console.WriteLine("Digite su apellido:");
+            apellido = Console.ReadLine();
+            Console.WriteLine("Su nombre completo es: " + nombre + apellido);   //Concatenacion
+            Console.WriteLine($"Su nombre completo es: {nombre}{apellido}");   //Interpolacion
+            Console.WriteLine("Su nombre completo es: {0}{1}", nombre, apellido); // formato de cadenas
+            Console.WriteLine("{0} Su nombre completo es: {1}", nombre, apellido); // formato de cadenas //se imprime dependiendo de donde estan los {}
+            byte edad;
+            float salario;
+            double estatura;
+
+            Console.WriteLine("digite su edad:");
+            edad = byte.Parse(Console.ReadLine());
+            Console.WriteLine("su nombre completo y su edad es: " + nombre + apellido + " " + edad);
+
+        }
+    }
+}
---
+++ 14 septiembre #1 PROGRA ll/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("14 septiembre #1 PROGRA ll")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("14 septiembre #1 PROGRA ll")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7d1e15d-a93d-407b-9ee1-c5e8c1ac3adc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
