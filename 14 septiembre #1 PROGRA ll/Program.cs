f1088602b0afa844855e273f2eb9eacbbaaddd13
alejandro020601 <alejandro.aguilar.arce@est.una.ac.cr>
2026-09-18 18:48:31 -0600

    Prueba de actualización de Program.cs

--- 14 septiembre #1 PROGRA ll/Program.cs
+++ 14 septiembre #1 PROGRA ll/Program.cs
@@ -10,8 +10,8 @@ namespace _14_septiembre__1_PROGRA_ll
     {
         static void Main(string[] args)
         {
-            int? num1 = null;
-
+            int num1 = 0;
+            int num2 = 10;
             int num3, num4;
             float flotante = 3.14f;
             string cadena = "Hola mundo";
@@ -36,13 +36,40 @@ namespace _14_septiembre__1_PROGRA_ll
             Console.WriteLine($"Su nombre completo es: {nombre}{apellido}");   //Interpolacion
             Console.WriteLine("Su nombre completo es: {0}{1}", nombre, apellido); // formato de cadenas
             Console.WriteLine("{0} Su nombre completo es: {1}", nombre, apellido); // formato de cadenas //se imprime dependiendo de donde estan los {}
+            
             byte edad;
             float salario;
             double estatura;
-
             Console.WriteLine("digite su edad:");
-            edad = byte.Parse(Console.ReadLine());
+            edad = byte.Parse(Console.ReadLine()); //=>"abc" ==> 52
+            salario = float.Parse(Console.ReadLine());
             Console.WriteLine("su nombre completo y su edad es: " + nombre + apellido + " " + edad);
+
+            Console.WriteLine("Digite un numero:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite otro numero:");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2) //numero 1 mayor que numero 2
+            {
+                Console.WriteLine($"El numero mayor es {num1}");
+            }
+            else if (num1 < num2) //numero 2 mayor que numero 1
+            {
+                Console.WriteLine($"El numero mayor es {num2}");
+            }
+            else
+            {
+                Console.WriteLine($" El numero {num1} es igual");
+            }
+            float resultado = (num1 + num2);
+            Console.WriteLine("La suma es: {0}", resultado);
+            resultado = (num1 - num2);
+            Console.WriteLine("La resta es: {0}", num1 - num2);
+            Console.WriteLine("multiplicacion: {0}", num1 * num2);
+            Console.WriteLine("La division es: {0}", (float)num1 / num2);
+
+            //Actualizacion
 
         }
     }
